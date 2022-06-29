1a18da6621c5fd2c650ba09ea62df7bad2d8c805
Junior Mendez <72240716+Junior-mendez@users.noreply.github.com>
2022-06-29 17:47:55 -0500

    fix: Se elimina importación sin usar.

--- src/test/java/com/co/pragma/serviceclienteimagen/ServiceClienteImagenApplicationTests.java
+++ src/test/java/com/co/pragma/serviceclienteimagen/ServiceClienteImagenApplicationTests.java
@@ -1,7 +1,6 @@
 package com.co.pragma.serviceclienteimagen;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 
